3083970505b7dad936c62eb134fdabd9183825c1
Jagermeister56 <jamesiasenza@gmail.com>
2023-12-17 23:39:44 -0500

    Update deprecation.h

--- src/deprecation.h
+++ src/deprecation.h
@@ -9,13 +9,12 @@
 // Deprecation policy:
 // * Shut down 16 weeks' worth of blocks after the estimated release block height.
 // * A warning is shown during the 2 weeks' worth of blocks prior to shut down.
-static const int APPROX_RELEASE_HEIGHT = 1208650; // Aug 2023
-static const int WEEKS_UNTIL_DEPRECATION = 16;
-static const int DEPRECATION_HEIGHT = APPROX_RELEASE_HEIGHT + (WEEKS_UNTIL_DEPRECATION * 7 * 24 * 24);
+static const int APPROX_RELEASE_HEIGHT = 1273100; // Dec 2023
+static const int WEEKS_UNTIL_DEPRECATION = 52;
+static const int DEPRECATION_HEIGHT = APPROX_RELEASE_HEIGHT + (WEEKS_UNTIL_DEPRECATION * 7 * 52 * 52);
 
 // Number of blocks before deprecation to warn users
-static const int DEPRECATION_WARN_LIMIT = 14 * 24 * 24; // 2 weeks
-
+static const int DEPRECATION_WARN_LIMIT = 14 * 48 * 48; // 4 weeks
 /**
  * Checks whether the node is deprecated based on the current block height, and
  * shuts down the node with an error if so (and deprecation is not disabled for
